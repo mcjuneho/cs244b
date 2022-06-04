9d44fe2a0a79c15c1b8eafe395a6ad075eaec6d2
michael Chang <michaelchang@DNa1dc4af.SUNet>
2022-06-03 20:19:17 -0700

    added improved implementation of RAFT

--- src/client.py
+++ src/client.py
@@ -1,8 +1,11 @@
+from urllib.request import parse_keqv_list
 import pip._vendor.requests as requests
 import time
 import imageio.v3 as iio
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import random
+import sys
 
 class Client():
     def __init__(self, name, ports) -> None:
@@ -13,9 +16,17 @@ class Client():
     def connect(self, server_index):
         self.__connected = 'http://localhost:' + self.__serverports[server_index]
 
-    def stream_data(self, name, data):
-        headers = {"Content-type": "application/octet-stream"}
-        requests.post(self.__connected + '/' + name, data = data, headers = headers)
+    def stream_data(self, name, data, size, index):
+        headers = {"Content-type": "application/octet-stream", "size": str(size), "index": str(index)}
+        r = requests.post(self.__connected + '/' + name, data = data, headers = headers)
+        if r.status_code == '400':
+            return None
+        return r.headers.get("parts-needed")
+
+    def start_stream(self, name, size):
+        headers = {"Content-type": "application/octet-stream", "size": str(size), "index": "-1"}
+        r = requests.post(self.__connected + '/' + name, data = b'', headers = headers)
+        return r.headers.get("parts-needed")
 
     def get_data(self, name):
         headers = {"Content-type": "application/octet-stream"}
@@ -26,6 +37,8 @@ class Client():
         return(self.__name)
 
 def main():
+    partition_chance = float(sys.argv[1])
+    print("partition chance: " + str(partition_chance))
     servers_ports = ['9200', '9300']
 
     client1 = Client('client1', servers_ports)
@@ -35,15 +48,60 @@ def main():
     client2.connect(1)
 
     #start streaming and retrieving data
-    img = iio.imread('../photos/stanford.jpeg')
+    img = iio.imread('../photos/stanford_big.jpeg')
     jpg_encoded = iio.imwrite("<bytes>", img, extension=".jpeg")
-    client1.stream_data('test', jpg_encoded)
-    time.sleep(0.5)
+    print(len(jpg_encoded))
 
-    jpg_recieved = client2.get_data('test')
-    img_received = iio.imread(jpg_recieved)
-    imgplot = plt.imshow(img_received)
-    plt.show()
+    size = len(jpg_encoded)
+    packets = []
+    num_packets = size // 1000
+    print(num_packets)
+    #return
+    #num_packets = 300
+
+    for i in range(num_packets-1):
+        #print(size//num_packets * i)
+        packets.append(jpg_encoded[size//num_packets * i: size//num_packets * (i+1)])
+    print(size//num_packets * (num_packets-1))
+    packets.append(jpg_encoded[size//num_packets * (num_packets-1):])
+
+    round = 0
+    partitions = 0
+    #partition_chance = 0.1
+
+    start = time.time()
+
+    packets_left = eval(client1.start_stream('test' + str(partitions), num_packets))
+
+    while len(packets_left) != 0:
+        print("round " + str(round))
+        print("packets left: " + str(packets_left))
+        if random.random() < partition_chance:
+            print("network partition " + str(partitions))
+            partitions += 1
+            packets_left = eval(client1.start_stream('test' + str(partitions), num_packets))
+            print(packets_left)
+
+        round += 1
+
+        for i in packets_left:
+            new_packets_left = client1.stream_data('test' + str(partitions), packets[i], num_packets, i)
+            if new_packets_left is not None:
+                packets_left = min(eval(new_packets_left), packets_left)
+
+        #sleep for 60 mils to simulate network rtt
+        time.sleep(0.06)
+
+    end = time.time()
+
+    print(end-start)
+    
+    #time.sleep(3)
+
+    #jpg_recieved = client2.get_data('test' + str(partitions))
+    #img_received = iio.imread(jpg_recieved)
+    #imgplot = plt.imshow(img_received)
+    #plt.show()
 
     #print(client2.get_data('test'))
 
--- src/server.py
+++ src/server.py
@@ -1,6 +1,9 @@
 from __future__ import print_function
+from pickletools import read_unicodestring1
 import sys
 import time
+import random
+from tokenize import Double
 try:
     from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 except ImportError:
@@ -27,13 +30,37 @@ class KVStorage(SyncObj):
         return self.__data.get(key, None)
 
 _g_kvstorage = None
+assembler_storage = {}
+drop_chance = 0.01
+
+class Assembler():
+    def __init__(self, size):
+        self.__size = size
+        self.__needed = set([i for i in range(size)])
+        self.__data = [None for i in range(size)]
+
+    def recieve(self, index, chunk):
+        if index in self.__needed:
+            self.__data[index] = chunk
+            self.__needed.remove(index)
+
+        return self.__needed
+
+    def finished(self):
+        return len(self.__needed) == 0
+
+    def get_needed(self):
+        return self.__needed
+
+    def reassemble(self):
+        return b''.join(self.__data)
 
 
 class KVRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         try:
             print("got get request with value: %s" % self.path)
-            value = _g_kvstorage.get(self.path)
+            value = _g_kvstorage.get(self.path).reassemble()
             #print(value)
 
             if value is None:
@@ -56,20 +83,55 @@ class KVRequestHandler(BaseHTTPRequestHandler):
         try:
             print("got post request with value: %s" % self.path)
             key = self.path
-            #print(key)
             #value = self.rfile.read(int(self.headers.get('content-length'))).decode()
             value = self.rfile.read(int(self.headers.get('content-length')))
-            #print(value)
-            _g_kvstorage.set(key, value)
+            print("read value")
+
+            #get size of the packet here
+            size = int(self.headers.get('size'))
+            index = int(self.headers.get('index'))
+            needed = set(i for i in range(size))
+
+            if random.random() < drop_chance and index != -1:
+                self.send_response(400)
+                self.end_headers()
+                print("sent response")
+                return
+
+            #print(size)
+            #print(index)
+            #print(needed)
+
+            if key not in assembler_storage:
+                #print("got to start of assembler")
+                image_assembler = Assembler(size)
+                #print("made image assembler")
+                assembler_storage[key] = image_assembler
+                #print("added assembler to storage")
+            elif index != -1:
+                #print("got to start of non-start assembler")
+                needed = assembler_storage[key].recieve(index, value)
+                #print("added data to assembler")
+                if len(needed) == 0:
+                    print("adding completed packet to kvs")
+                    _g_kvstorage.set(key, assembler_storage[key])
+                    print("succesfully added complete assembler to kvs")
+                    assembler_storage.pop(key)
+
+            #print("sending response")
             self.send_response(201)
             self.send_header("Content-type", "application/octet-stream")
+            self.send_header("parts-needed", str(needed))
             #self.send_header("Content-type", "text/plain")
             self.end_headers()
         except:
             pass
 
-
 def main():
+    global _g_kvstorage
+    global assembler_storage
+    global drop_chance
+
     if len(sys.argv) < 3:
         print('Usage: %s http_port dump_file.bin selfHost:port partner1Host:port partner2Host:port ...' % sys.argv[0])
         sys.exit(-1)
@@ -80,17 +142,15 @@ def main():
 
     httpPort = int(sys.argv[1])
     print(httpPort)
-    partners = ['localhost:%d' % int(p) for p in sys.argv[2:]]
+    drop_chance = float(sys.argv[2])
+    print("drop chance: " + str(drop_chance))
+    partners = ['localhost:%d' % int(p) for p in sys.argv[3:]]
 
-    global _g_kvstorage
     _g_kvstorage = KVStorage('localhost:%d' % httpPort, partners)
     while True:
         time.sleep(0.5)
 
         if _g_kvstorage._getLeader() is not None:
-            #_g_kvstorage.set('test', 2)
-            #print(_g_kvstorage.get('test'))
-            #print(_g_kvstorage.get_counter())
             print("Servers are synced")
             break
    
